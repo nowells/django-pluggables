1ba62bb3bb0c3c2986ea7ab941892fd7b3fad109
Tareque Hossain <tareque@codexn.com>
2009-09-04 11:31:15 -0400

    Added database settings.

--- examples/funnywalks/settings.py
+++ examples/funnywalks/settings.py
@@ -4,3 +4,13 @@ INSTALLED_APPS = (
     'complaints',
     'funnywalks',
 )
+
+import os
+DB_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'db'))
+DATABASE_NAME = os.path.join(DB_ROOT, 'database.db')
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+DATABASE_OPTIONS = {}
