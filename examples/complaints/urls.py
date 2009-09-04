df1e2468f9d0fd983df4fcbbbd9e14d0c0725021
Nowell Strite <nowell@strite.org>
2009-09-04 11:56:33 -0400

    More updates.

--- examples/complaints/urls.py
+++ examples/complaints/urls.py
@@ -4,7 +4,7 @@ from complaints import views
 
 class Complaints(Pluggable):
     urlpatterns = patterns('',
-        url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'complaints/index.html'}),
+        url(r'^$', views.index),
         url(r'^create/$', views.edit),
         url(r'^(?P<complaint_id>\d+)/edit/$', views.edit),
     )
--- examples/sillywalks/models.py
+++ examples/sillywalks/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class SillyWalk(models.Model):
+    name = models.SlugField(unique=True)
     description = models.TextField()
     video_url = models.URLField()
 
     def __unicode__(self):
-        return self.description
+        return self.name
