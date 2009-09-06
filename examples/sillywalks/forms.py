8fe9ba0c769a15bf52e7274d49a2025ac46823de
Nowell Strite <nowell@strite.org>
2009-09-06 09:40:39 -0400

    Added edit and create to silly walks. Site needs a style overhaul. And a delete sillywalk method to round it all out.

---
+++ examples/sillywalks/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from sillywalks.models import SillyWalk
+
+class SillyWalkForm(forms.ModelForm):
+    class Meta:
+        model = SillyWalk
--- examples/sillywalks/views.py
+++ examples/sillywalks/views.py
@@ -1,9 +1,10 @@
-from django.core.urlresolvers import resolve
-from django.http import Http404
+from django.core.urlresolvers import resolve, reverse
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from complaints.views import Complaints
+from sillywalks.forms import SillyWalkForm
 from sillywalks.models import SillyWalk
 
 def index(request):
@@ -14,6 +15,25 @@ def index(request):
 
 def view(request, walk_name):
     return complaints.index(request, walk_name=walk_name)
+
+def edit(request, walk_name=None):
+    try:
+        sillywalk = SillyWalk.objects.get(name=walk_name)
+    except SillyWalk.DoesNotExist:
+        sillywalk = None
+
+    if request.method == 'POST':
+        form = SillyWalkForm(request.POST, instance=sillywalk)
+        if form.is_valid():
+            sillywalk = form.save()
+            return HttpResponseRedirect(reverse('sillywalks_view', args=[sillywalk.name]))
+    else:
+        form = SillyWalkForm(instance=sillywalk)
+
+    return render_to_response('sillywalks/edit.html', {
+        'sillywalk': sillywalk,
+        'form': form,
+        }, context_instance=RequestContext(request))
 
 class SillyWalkComplaints(Complaints):
     def pluggable_config(self, request, walk_name=None):
