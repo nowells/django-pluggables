9621b5407f4275197f4f1a9c14a1811e86beb434
Nowell Strite <nowell@strite.org>
2009-09-08 11:24:49 -0700

    Fixed setup.py script.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.1.2'
 
 setup(
     name='django-pluggables',
     version=version,
     description='A design pattern for Django that allows you to build "Pluggable" Reusable Applications',
-    long_description=open('docs/overview.rst').read(),
     author='Nowell Strite',
     author_email='nowell@strite.org',
     url='http://github.com/nowells/django-pluggables/',
-    packages=['pluggables'],
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
