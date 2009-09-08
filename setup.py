170feadc949bfed30c7ccff61c8f4038eaf6d2c3
Nowell Strite <nowell@strite.org>
2009-09-08 11:35:41 -0700

    I really need to work on my setup.py skills.

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.1.2'
+version = '0.1.3'
 
 setup(
     name='django-pluggables',
     version=version,
     description='A design pattern for Django that allows you to build "Pluggable" Reusable Applications',
+    long_description='A design pattern for Django that allows you to build "Pluggable" Reusable Applications',
     author='Nowell Strite',
     author_email='nowell@strite.org',
     url='http://github.com/nowells/django-pluggables/',
     packages=find_packages(),
+    zip_safe=False,
+    platforms=["any"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
