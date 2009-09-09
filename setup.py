3085d6a4f8a00ee7670e88af753ba476e9659b61
Nowell Strite <nowell@strite.org>
2009-09-09 09:35:20 -0700

    Updated setup.py description.

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ setup(
     name='django-pluggables',
     version=version,
     description='A design pattern for Django that allows you to build "Pluggable" Reusable Applications',
-    long_description='django-pluggables is a design pattern for Django that allows you to build "Pluggable" Reusable Applications',
+    long_description="""Django-Pluggables is a design pattern that endows reusable applications with a few additional features:
+
+#. Applications can exist at multiple URL locations (e.g. http://example.com/foo/app/ and http://example.com/bar/app/).
+#. Applications can be "parented" to other applications or objects which can then deliver specialized context information.
+#. Posting form data and error handling can happen in locations that make sense to the user, as opposed to the common practice of using templatetags and standalone error or preview pages for form data processing.
+#. Views and templates remain generic and reusable.""",
     author='Nowell Strite',
     author_email='nowell@strite.org',
     url='http://github.com/nowells/django-pluggables/',
